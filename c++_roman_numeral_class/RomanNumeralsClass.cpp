c619456a9a18482af072166aa9da97d34e17984f
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2022-02-18 11:00:50 -0500

    Just realized I could delete a bunch of numbers that would've never been used lol

--- c++_roman_numeral_class/RomanNumeralsClass.cpp
+++ c++_roman_numeral_class/RomanNumeralsClass.cpp
@@ -25,7 +25,6 @@ public:
     std::string to_roman(unsigned int n) {
         std::map<int, std::string> characters = { //I should've refactored the name
       { 1, "I" },{ 2, "II"},{3, "III"},{4, "IV"},{ 5, "V" },{6, "VI"},{7, "VII"},{8, "VIII"},{9, "IX"},{ 10, "X" },
-      {11, "XI"},{12, "XII"},{13, "XIII"},{14, "XIV"},{15, "XV"},{16, "XVI"},{17, "XVII"},{18, "XVIII"},{19, "XIX"},
       {40, "XL"},{50, "L"},{90, "XC"},{100, "C"},{400, "CD"},{500, "D"},{900, "CM"},{1000, "M"}
         };
 
